73525e5cdbecde715f53e46fc9d84876e20f1c67
dvuvud <carldavid.buhrman@gmail.com>
2026-04-11 18:38:58 +0200

    add static macro factory

--- src/core/Macro.hpp
+++ src/core/Macro.hpp
@@ -1,9 +1,20 @@
+#pragma once
+
 #include "MacroAction.hpp"
 #include <vector>
 #include <string>
+#include <QUuid>
 
 struct Macro {
     std::string              id;
     std::string              name;
     std::vector<MacroAction> actions;
+
+    static Macro create(const std::string& name = "New Macro") {
+        return {
+            QUuid::createUuid().toString(QUuid::WithoutBraces).toStdString(),
+            name,
+            {}
+        };
+    }
 };
